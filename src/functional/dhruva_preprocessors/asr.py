ac36e28b3e4ca2afe58f08eb655a45f3eeadd65d
Ashwin <ashwinsrinivasan@ai4bharat.org>
2023-07-04 14:50:16 +0000

    minor changes

--- src/functional/dhruva_preprocessors/asr.py
+++ src/functional/dhruva_preprocessors/asr.py
@@ -2,11 +2,10 @@ from .speech_transcript_cleaning import cleaning_pipeline, get_dict_chars
 
 
 def clean_and_normalize_transcripts(raw_data):
-    # print(raw_data)
-    # dict_characters = get_dict_chars(raw_data["language"])
-    # no_ood, preprocessed_sentence = cleaning_pipeline(
-    #     dict_characters, raw_data["transcript"], raw_data["language"]
-    # )
-    # raw_data["transcript"] = preprocessed_sentence
+    dict_characters = get_dict_chars(raw_data["language"])
+    no_ood, preprocessed_sentence = cleaning_pipeline(
+        dict_characters, raw_data["transcript"], raw_data["language"]
+    )
+    raw_data["transcript"] = preprocessed_sentence
     return raw_data
     # return {"audio": raw_data["audio"], "transcript": preprocessed_sentence, "language": raw_data["language"]}
